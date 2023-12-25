a9100886d43e51c63b858841430b5daff6484465
Krishna11118 <krishnassss365@gmail.com>
2023-12-25 17:41:58 +0530

    updates

--- backend/routes/reviews.js
+++ backend/routes/reviews.js
@@ -4,6 +4,6 @@ import { verifyUser } from "../utils/verifyToken.js";
  
 const router = express.Router();
 
-router.post("/:tourId", verifyUser, createReview);
+router.post("/:tourId",  createReview);
 
 export default router;
