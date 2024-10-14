a0f6742258954ff89901233bab77e6251c2e062f
Krishna . <krishnassss365@gmail.com>
2024-10-14 12:15:09 +0000

    addmingDotenv

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,4 +1,5 @@
 // export const BASE_URL = 'http://localhost:8000/api/v1'
 
-export const BASE_URL = 'https://trekki-backend.onrender.com/api/v1'
+// export const BASE_URL = 'https://trekki-backend.onrender.com/api/v1'
 
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL;
