3a6101dc7b5a2dbc9e54fdddd1b2c1fe0085ba4a
Krishna . <krishnassss365@gmail.com>
2024-10-15 05:50:52 +0000

    fontend>envBanckendURL

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -3,3 +3,6 @@
 // export const BASE_URL = 'https://trekki-backend.onrender.com/api/v1'
 
 export const BASE_URL = process.env.REACT_APP_BACKEND_URL;
+
+// export const BASE_URL = "https://3001-krishna11118-trekki-i2zin2ai74x.ws-us116.gitpod.io/api/v1";
+
